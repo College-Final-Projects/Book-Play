12684eadbcad2c95436017f1a93fed9db79cce2c
Bakir-Hage <bakir.ah2@gmail.com>
2025-03-14 00:09:48 +0200

    Revert "صلحت الي اتفقنا عليه ومضفتش ولا اشي"
    This reverts commit 031de823da6843f2fb83fee3877a456021e6a812.

--- User_Selection_Page/UserSelection.js
+++ User_Selection_Page/UserSelection.js
@@ -1,15 +1,15 @@
 console.log("JavaScript Loaded");
-
 function redirectTo(page) {
     window.location.href = page;
 }
-
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("player").addEventListener("click", function () {
-        window.location.href = "../Home Page/player/PlayerDashboard.php"; 
+        window.location.href = "../Home Page/player/PlayerDashboard.html"; 
+
     });
 
     document.getElementById("facilityOwner").addEventListener("click", function () {
         window.location.href = "../Home Page/Facility_Owner/FacilityDashboard.php";
+
     });
-});
+
