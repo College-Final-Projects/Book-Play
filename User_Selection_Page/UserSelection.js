22cca8e2f12d1ef2c112aadb5238e00c611914f0
christianhaddad25104 <chris.fady.h@gmail.com>
2025-03-13 13:01:35 +0200

    خبصت

--- User_Selection_Page/UserSelection.js
+++ User_Selection_Page/UserSelection.js
@@ -2,14 +2,14 @@ console.log("JavaScript Loaded");
 function redirectTo(page) {
     window.location.href = page;
 }
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("player").addEventListener("click", function () {
-        window.location.href = "../Home Page/player/PlayerDashboard.html"; 
+document.getElementById("player").addEventListener("click", function () {
+    sessionStorage.setItem("selectedUser", "player");
+    window.location.href = "../Home Page/player/PlayerProfile.html";
+});
 
-    });
 
     document.getElementById("facilityOwner").addEventListener("click", function () {
         window.location.href = "../Home Page/Facility_Owner/FacilityDashboard.html";
 
     });
-});
+
