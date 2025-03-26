a7f8f2d19e1888c989864829a9c06f63afe50c40
Bakir-Hage <bakir.ah2@gmail.com>
2025-03-26 08:43:44 +0200

    مزبطش معي

--- mail/MailLink.php
+++ mail/MailLink.php
@@ -1,15 +1,60 @@
 <?php
+require 'vendor/autoload.php';
+include "../config/connection.php";
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php'; // If installed via Composer
-// require 'path_to_phpmailer/src/PHPMailer.php'; // If manually added
-// require 'path_to_phpmailer/src/SMTP.php';
-// require 'path_to_phpmailer/src/Exception.php';
-
 $mail = new PHPMailer(true);
 
-try {
+header('Content-Type: application/json');
+
+if (isset($_POST["sendCodeButton"])) {
+    $email = $_POST["email"];
+    $usersRef = $database->getReference('users')->getValue();
+
+    $userFound = false;
+    $authenticationCode = mt_rand(10000, 99999);
+
+    foreach ($usersRef as $key => $value) {
+        if ($email === $value['email']) {
+            $userFound = true;
+            $username = $value["username"];
+            
+            try {
+                // إعداد البريد
+                $mail = new PHPMailer(true);
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'bookandplay.team@gmail.com';
+                $mail->Password = 'yxfb ifsl szkz phma';
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 587;
+    
+                $mail->setFrom('bookandplay.team@gmail.com', 'Book And Play Team');
+                $mail->addAddress($value["email"], $value["username"]);
+    
+                $authenticationCode = mt_rand(10000, 99999);
+    
+                $mail->isHTML(true);
+                $mail->Subject = 'Your Verification Code';
+                $mail->Body = 'Your verification code is: <b>' . $authenticationCode . '</b>';
+                $mail->AltBody = 'Your verification code is: ' . $authenticationCode;
+    
+                $mail->send();
+    
+                $database->getReference("users/$key/code")->set($authenticationCode);
+                echo json_encode(["success" => true, "message" => "Code sent successfully!"]);
+                exit;
+            }catch (Exception $e) {
+                echo json_encode(["success" => false, "message" => "Error sending email: " . $mail->ErrorInfo]);
+                exit;
+            }
+        }
+    }
+}
+/*try {
     // SMTP Configuration
     $mail->isSMTP();
     $mail->Host       = 'smtp.gmail.com';
@@ -29,5 +74,5 @@ try {
     echo 'Email has been sent successfully!';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+}*/
 ?>
