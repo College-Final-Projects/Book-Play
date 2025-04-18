08045f6d44cd8e41e1d311f70e1e8f9d5196eb99
christianhaddad25104 <chris.fady.h@gmail.com>
2025-04-18 15:29:11 +0300

    :Admin Page Done 100%

---
+++ Admin/ManageVenueRequests/ManageVenueRequests.js
@@ -0,0 +1,9 @@
+function openModal(id) {
+    document.getElementById('detailsModal').style.display = 'flex';
+    // في المستقبل: يمكن استخدام id لجلب البيانات من قاعدة البيانات
+  }
+  
+  function closeModal() {
+    document.getElementById('detailsModal').style.display = 'none';
+  }
+  
